bcffa0cda76916cee3ecfdb73beb8fb16ec37227
mirhakhan38 <mirhakhan728@gmail.com>
2025-03-01 12:13:10 +0400

    exercise1.py

---
+++ Assesment 1/Exercise1.py
@@ -0,0 +1,8 @@
+#assigning value to variable 1
+word1 = "coding"
+#assigning value to variable 2
+word2 = "is"
+#assigning the value to variable 3
+word3 = "cool"
+#use string concatentation to combine the variables and print the values
+print(word1 +" "+ word2 +" "+word3)
