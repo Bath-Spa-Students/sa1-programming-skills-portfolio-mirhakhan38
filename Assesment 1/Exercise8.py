1b41983d480e629fe667f52f3c952da4d9a489f5
mirhakhan38 <mirhakhan728@gmail.com>
2025-03-15 15:40:41 +0400

    exercise8.py

---
+++ Assesment 1/Exercise8.py
@@ -0,0 +1,18 @@
+#list of names
+Names=["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+#searching for Sam
+Target="Sam"
+#Check if the name is present or not
+if Target in Names:
+    print(f"{Target} found in the list!")
+else:
+    print(f"Oops! {Target} not found in the list")
+
+#Optional requiremnets
+#Allow the user to input the search
+Target=input("Search for a name!")
+#Check if the name is present or not
+if Target in Names:
+    print(f"{Target} found in the list!")
+else:
+    print(f"Oops! {Target} not found in the list")
