2e6b04ea5dade1ff9d759a7f878557c1ee9041be
mirhakhan38 <mirhakhan728@gmail.com>
2025-03-15 16:19:23 +0400

    Exercise4.py

---
+++ Assesment 1/Exercise4.py
@@ -0,0 +1,8 @@
+#ask user the question
+answer=input("What is th ecapital of France?")
+#Check if the answer is correct
+if answer.strip().lower()=="Paris":
+    print(f"Correct! The capital of France is Paris.")
+else: 
+    print(f"Wrong! The capital of France is Pairs")
+
