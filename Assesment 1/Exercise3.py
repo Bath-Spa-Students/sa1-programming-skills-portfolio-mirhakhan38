aa7688d07590f038d0f7ee13f8bc071d1f60a39b
mirhakhan38 <mirhakhan728@gmail.com>
2025-03-22 22:43:14 +0400

    Exercise3.py

---
+++ Assesment 1/Exercise3.py
@@ -0,0 +1,8 @@
+#my biography
+my_data={
+    "Name":"Mirha Khan",
+    "Age": 18,
+    "Hometown": "Karachi",
+}
+#printing values on separate lines using a single print() statement
+print(*my_data, sep="\n")
