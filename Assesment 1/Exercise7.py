11ad4786dd09fe4c40eb3719472ac63750f48ed1
mirhakhan38 <mirhakhan728@gmail.com>
2025-04-04 21:41:38 +0400

    Exercise7.py

---
+++ Assesment 1/Exercise7.py
@@ -0,0 +1,15 @@
+#loop that counts up from 0 to 50 in increments of 1
+for a in range(51):
+ print(a)
+# loop that counts down from 50 to 0 in decrements of 1
+for b in range(50, -1, -1):
+  print(b)
+#loop that counts up from 30 to 50 in increments of 1
+for c in range (30, 51):
+   print(c)
+#loop that counts down from 50 to 10 in decrements of 2
+for d in range(9, 50, -2):
+ print(d)
+#loop that counts up from 100 to 200 in increments of 5
+for e in range(100, 200, 5):
+  print(e)
